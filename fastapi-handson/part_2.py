d2c028ab23c987f756f469797e72d8f212f8770e
sam99dave <sam99dave@gmail.com>
2023-09-09 18:44:57 +0530

    Body - Nested Models

--- fastapi-handson/part_2.py
+++ fastapi-handson/part_2.py
@@ -1,11 +1,12 @@
 """
 -> Body - Multiple Parameters
-
+-> Body - Fields
+-> Body - Nested Models
 """
-from typing import Union
+from typing import Union, Set, List, Dict
 
 from fastapi import FastAPI, Path, Body
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, HttpUrl
 from typing_extensions import Annotated
 
 app = FastAPI()
@@ -62,3 +63,100 @@ Learn about the usecase at -> https://fastapi.tiangolo.com/tutorial/body-multipl
 async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
     results = {"item_id": item_id, "item": item}
     return results
+
+# Body - Fields
+class ItemNew(BaseModel):
+    name: str
+    description: Union[str, None] = Field(
+        default=None, title="The description of the item", max_length=300
+    )
+    price: float = Field(gt=0, description="The price must be greater than zero")
+    tax: Union[float, None] = None
+
+@app.put("/body-field/{item_id}")
+async def update_item(item_id: int, item: Annotated[ItemNew, Body(embed=True)]):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+# Body - Nested Models
+
+class Image(BaseModel):
+    url: str
+    name: str
+
+class ItemNested(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    tags: Set[str] = set()
+    image: Union[Image, None] = None
+
+@app.put("/nested-models/{item_id}")
+async def update_item(item_id: int, item: ItemNested):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+"""
+Here in the above code snippet, the sub-model -> Image
+is used as a type for attribute image in ItemNested
+"""
+
+## Special types and validation
+
+class ImageSpecial(BaseModel):
+    url: HttpUrl
+    name: str
+
+class ItemSpecial(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    tags: Set[str] = set()
+    image: Union[ImageSpecial, None] = None
+
+@app.put("/special-types/{item_id}")
+async def update_item(item_id: int, item: ItemSpecial):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+## Deeply Nested Models
+
+class ImageDeep(BaseModel):
+    url: HttpUrl
+    name: str
+
+class ItemDeep(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    tags: Set[str] = set()
+    images: Union[List[ImageDeep], None] = None
+
+class Offer(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    items: List[ItemDeep]
+
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    return offer
+
+
+## Bodies of arbitrary dicts
+"""
+In this case, you would accept any dict as long as it has int keys with float values:
+
+NOTE
+Have in mind that JSON only supports str as keys.
+But Pydantic has automatic data conversion.
+This means that, even though your API clients can only send strings as keys, as long as those strings contain pure integers, Pydantic will convert them and validate them.
+And the dict you receive as weights will actually have int keys and float values.
+"""
+@app.post("/index-weights/")
+async def create_index_weights(weights: Dict[int, float]):
+    return weights
